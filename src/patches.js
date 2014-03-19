b62a030e0235d747b8db1282e69e16cde480f948
Guy Kloss <gk@mega.co.nz>
2014-03-20 10:06:29 +1300

    #92 externalised some third party module patches to a separate module

---
+++ src/patches.js
@@ -0,0 +1,138 @@
+/**
+ * @fileOverview
+ * Some patches/enhancements to third party library modules.
+ */
+
+(function() {
+    "use strict";
+
+    /**
+     * @namespace
+     * Some patches/enhancements to third party library modules.
+     * 
+     * @description
+     * <p>Some patches/enhancements to third party library modules.</p>
+     */
+    
+    var _assert = mpenc.assert.assert;
+    
+    /*
+     * Created: 20 Mar 2014 Guy K. Kloss <gk@mega.co.nz>
+     *
+     * (c) 2014 by Mega Limited, Wellsford, New Zealand
+     *     http://mega.co.nz/
+     *
+     * This file is part of the multi-party chat encryption suite.
+     *
+     * This code is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License version 3
+     * as published by the Free Software Foundation. See the accompanying
+     * LICENSE file or <https://www.gnu.org/licenses/> if it is unavailable.
+     * 
+     * This code is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+     */
+    
+    // Patches to the curve255 namespace module.
+    
+    /**
+     * Converts an 16-bit word element (unsigned) array a hex string representation.
+     * 
+     * @param key
+     *     The key as an 8 bit (unsigned) integer array.
+     * @returns
+     *     Hex string representation of key (big endian).
+     * @private
+     */
+    curve255.toHex = function(key) {
+        var out = '';
+        for (var i = 0; i < key.length; i++) {
+            var value = key[i];
+            var remainder = 0;
+            for (var j = 0; j < 4; j++) {
+                remainder = value % 16;
+                out = mpenc.utils._HEX_CHARS[remainder % 0x0f] + out;
+                value = value >> 4;
+            }
+        }
+        return out;
+    };
+
+
+    /**
+     * Converts a hex string to a 16-bit word element (unsigned) array representation.
+     * 
+     * @param key
+     *     Hex string representation of key (big endian).
+     * @returns
+     *     The key as an 16-bit word element (unsigned) integer array.
+     * @private
+     */
+    curve255.fromHex = function(key) {
+        var out = [];
+        if (key.length % 4) {
+            var padding = 4 - key.length % 4;
+            for (var i = 0; i < padding; i++) {
+                key = '0' + key;
+            }
+        }
+        var i = 0;
+        while (i < key.length) {
+            var value = 0;
+            for (var j = 0; j < 4; j++) {
+                value = (value << 4) + mpenc.utils._HEX_CHARS.indexOf(key[i + j]);
+            }
+            out.unshift(value);
+            i += 4;
+        }
+        return out;
+    };
+
+
+    /**
+     * Converts an 16-bit word element (unsigned) array a binary string representation.
+     * 
+     * @param key
+     *     The key as an 16-bit word element (unsigned) integer array.
+     * @returns
+     *     Binary string representation of key (big endian).
+     * @private
+     */
+    curve255.toString = function(key) {
+        var out = '';
+        for (var i = 0; i < key.length; i++) {
+            var value = key[i];
+            var remainder = 0;
+            for (var j = 0; j < 2; j++) {
+                remainder = value % 256;
+                out = String.fromCharCode(remainder) + out;
+                value = value >> 8;
+            }
+        }
+        return out;
+    };
+
+
+    /**
+     * Converts a binary string to a 16-bit word element (unsigned) array representation.
+     * 
+     * @param key
+     *     Binary string representation of key (big endian).
+     * @returns
+     *     The key as an 16-bit word element (unsigned) integer array.
+     * @private
+     */
+    curve255.fromString = function(key) {
+        var out = [];
+        var i = 0;
+        if (key.length % 2) {
+            key = '\u0000' + key;
+        }
+        while (i < key.length) {
+            out.unshift(key.charCodeAt(i) * 256 + key.charCodeAt(i + 1));
+            i += 2;
+        }
+        return out;
+    };
+})();
---
+++ test/patches_test.js
@@ -0,0 +1,76 @@
+/**
+ * @fileOverview
+ * Tests for `patches` module.
+ */
+
+/*
+ * Created: 20 Mar 2014 Guy K. Kloss <gk@mega.co.nz>
+ *
+ * (c) 2014 by Mega Limited, Wellsford, New Zealand
+ *     http://mega.co.nz/
+ *
+ * This file is part of the multi-party chat encryption suite.
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation. See the accompanying
+ * LICENSE file or <https://www.gnu.org/licenses/> if it is unavailable.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+(function() {
+    "use strict";
+    
+    var assert = chai.assert;
+
+    describe("curve255 patches", function() {
+        describe('(toHex())', function() {
+            it('simple conversion test', function() {
+                var values = [[722, 18838], [123], [123, 0], []];
+                var expected = ['499602d2', '007b', '0000007b', ''];
+                for (var i = 0; i < values.length; i++) {
+                    var result = curve255.toHex(values[i]);
+                    assert.strictEqual(result, expected[i]);
+                }
+            });
+        });
+        
+        describe('(fromHex())', function() {
+            it('simple conversion test', function() {
+                var values = ['499602d2', '007b', '0000007b', '000007b', '00007b', ''];
+                var expected = [[722, 18838], [123], [123, 0], [123, 0], [123, 0], []];
+                for (var i = 0; i < values.length; i++) {
+                    var result = curve255.fromHex(values[i]);
+                    assert.deepEqual(result, expected[i]);
+                }
+            });
+        });
+        
+        describe('(toString())', function() {
+            it('simple conversion test', function() {
+                var values = [[29556, 26209, 29300, 25185, 29801, 24946, 21356],
+                              [25187, 97], []];
+                var expected = ['Slartibartfast', '\u0000abc', ''];
+                for (var i = 0; i < values.length; i++) {
+                    var result = curve255.toString(values[i]);
+                    assert.strictEqual(result, expected[i]);
+                }
+            });
+        });
+        
+        describe('(fromString())', function() {
+            it('simple conversion test', function() {
+                var values = ['Slartibartfast', '\u0000abc', 'abc', ''];
+                var expected = [[29556, 26209, 29300, 25185, 29801, 24946, 21356],
+                                [25187, 97], [25187, 97], []];
+                for (var i = 0; i < values.length; i++) {
+                    var result = curve255.fromString(values[i]);
+                    assert.deepEqual(result, expected[i]);
+                }
+            });
+        });
+    });
+})();
