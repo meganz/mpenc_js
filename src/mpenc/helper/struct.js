6bc8bff431a3a437343c7925f3dbf48717e8ad14
Ximin Luo <xl@mega.co.nz>
2015-02-06 22:23:15 +0100

    ImmutableSet: refactor to look more like a classical JS class, whilst retaining encapsulation

--- src/mpenc/helper/struct.js
+++ src/mpenc/helper/struct.js
@@ -65,198 +65,190 @@ define([
     ns.iteratorToArray = iteratorToArray;
 
     /**
-     * An immutable set, implemented using sorted arrays. Does not scale to
-     * massive sizes, but should be adequate for representing (e.g.) members
-     * of a chat.
-     *
-     * <p>Equality in equals() is taken strictly, using <code>===</code>.</p>
-     *
-     * <p>Use as a <b>factory function</b> as in <code><del>new</del>
-     * ImmutableSet([1, 2, 3])</code>.</p>
-     *
-     * <p>Otherwise, the API is intended to match facebook's <a
+     * An immutable set, intended to match facebook's <a
      * href="https://github.com/facebook/immutable-js/">Immutable JS</a>
      * library. We don't use that, because it is 42kb and we only need Set.</p>
      *
+     * <p>Equality in equals() is taken strictly, using <code>===</code>. May
+     * be used as a factory method, without <code>new</code>.</p>
+     *
+     * <p>Does not scale to massive sizes, but should be adequate for
+     * representing (e.g.) members of a chat.</p>
+     *
+     * @class
      * @param {...*} ... Elements of the set
-     * @returns {module:mpenc/helper/struct.ImmutableSet}
-     * @memberOf! module:mpenc/helper/struct#
+     * @memberOf! module:mpenc/helper/struct
      */
     var ImmutableSet = function(iterable) {
+        if (!(this instanceof ImmutableSet)) return new ImmutableSet(iterable);
 
         var items = new Set(iterable);
 
-        /** @lends module:mpenc/helper/struct.ImmutableSet.prototype */
-        var self = Object.create(ImmutableSet.prototype);
-
         // facebook ImmutableSet provides length
-        self.length = items.size;
-        self.size = items.size;
+        this.length = items.size;
+        this.size = items.size;
 
         // adhere to the Iterable interface if available
         if (typeof Symbol !== "undefined") {
-            self[Symbol.iterator] = function() {
+            // ES6 current standard
+            this[Symbol.iterator] = function() {
                 return items[Symbol.iterator]();
             };
-        }
-
-        // at time of writing, Firefox ESR (31) uses an older syntax
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of#Browser_compatibility
-        if ("@@iterator" in items) {
-            self["@@iterator"] = function() {
+        } else if ("@@iterator" in items) {
+            // at time of writing, Firefox ESR (31) uses an older syntax
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of#Browser_compatibility
+            this["@@iterator"] = function() {
                 return items["@@iterator"]();
             };
         }
-
-        /**
-         * Return an iterator over the elements of this set.
-         * @returns {Array}
-         */
-        self.asMutable = function() {
-            return new Set(items);
-        };
-
-        /**
-         * Return a sorted array representation of this set.
-         * @returns {Array}
-         */
-        self.toArray = function() {
-            var a = [];
-            items.forEach(function(v) { a.push(v); });
-            a.sort();
-            return a;
-        };
 
         /**
          * Apply a function to every member. The callback is the same as Set.
          * @param callback {forEachCallback} Function to execute for each element.
          * @param thisObj {} Value to use as <code>this</code> when executing <code>callback</code>.
          */
-        self.forEach = function(callback, thisObj) {
+        this.forEach = function(callback, thisObj) {
             return items.forEach(function(v, v0, a) {
-                // prevent acccess to mutable set
-                return callback.call(thisObj, v, v0, self);
+                // prevent external acccess to mutable set
+                return callback.call(thisObj, v, v0, this);
             });
-        };
-
-        /**
-         * Return a string representation of this set.
-         * @returns {string}
-         */
-        self.toString = function() {
-            return "ImmutableSet(" + self.toArray() + ")";
         };
 
         /**
          * Whether the set contains the given element.
          * @returns {boolean}
          */
-        self.has = function(elem) {
+        this.has = function(elem) {
             return items.has(elem);
         };
-
-        /**
-         * Return whether this set equals another set.
-         * @param {module:mpenc/helper/struct.ImmutableSet} other
-         * @returns {boolean}
-         */
-        self.equals = function(other) {
-            if (!other || other.size !== self.size) {
-                return false;
-            }
-            var eq = true;
-            items.forEach(function(v) {
-                if (!other.has(v)) {
-                    eq = false;
-                }
-            });
-            return eq;
-        };
-
-        /**
-         * Return the disjunction of this and another set, i.e. elements that
-         * are in this <b>or</b> the other set.
-         * @param {module:mpenc/helper/struct.ImmutableSet} other
-         * @returns {module:mpenc/helper/struct.ImmutableSet}
-         */
-        self.union = function(other) {
-            var union = other.asMutable();
-            items.forEach(function(v) {
-                union.add(v);
-            });
-            return ImmutableSet(union);
-        };
-
-        /**
-         * Return the conjunction of this and another set, i.e. elements that
-         * are in this <b>and</b> the other set.
-         * @param {module:mpenc/helper/struct.ImmutableSet} other
-         * @returns {module:mpenc/helper/struct.ImmutableSet}
-         */
-        self.intersect = function(other) {
-            var intersection = new Set();
-            items.forEach(function(v) {
-                if (other.has(v)) {
-                    intersection.add(v);
-                }
-            });
-            return ImmutableSet(intersection);
-        };
-
-        /**
-         * Return the difference of this and another set, i.e. elements that
-         * are in this <b>and not</b> the other set.
-         * @param {module:mpenc/helper/struct.ImmutableSet} other
-         * @returns {module:mpenc/helper/struct.ImmutableSet}
-         */
-        self.subtract = function(other) {
-            var difference = self.asMutable();
-            items.forEach(function(v) {
-                if (other.has(v)) {
-                    difference.delete(v);
-                }
-            });
-            return ImmutableSet(difference);
-        };
-
-        /**
-         * Return what was [added, removed] between this and another set, i.e.
-         * same as [other.subtract(this), this.subtract(other)].
-         * @param {module:mpenc/helper/struct.ImmutableSet} newer
-         * @returns {module:mpenc/helper/struct.ImmutableSet[]}
-         */
-        self.diff = function(newer) {
-            return [newer.subtract(self), self.subtract(newer)];
-        };
-
-        /**
-         * Apply a difference to an older set.
-         *
-         * @param older {module:mpenc/helper/struct.ImmutableSet} Older set
-         * @param diff {module:mpenc/helper/struct.ImmutableSet[]} 2-tuple of what to (add, remove).
-         * @returns {module:mpenc/helper/struct.ImmutableSet} Newer set
-         */
-        self.patch = function(diff) {
-            if (!diff || diff[0].intersect(diff[1]).size > 0) {
-                throw new Error("invalid diff: " + diff);
-            }
-            return self.union(diff[0]).subtract(diff[1]);
-        };
-
-        /**
-         * Do a 3-way merge between this parent set and two child sets.
-         * @param {module:mpenc/helper/struct.ImmutableSet} first child
-         * @param {module:mpenc/helper/struct.ImmutableSet} other child
-         * @returns {module:mpenc/helper/struct.ImmutableSet} Result set
-         */
-        self.merge = function(child0, child1) {
-            return child1.patch(self.diff(child0));
-        };
-
-        return self;
     };
-    /** @class
-     * @see module:mpenc/helper/struct#ImmutableSet */
+
+    /**
+     * Return a sorted array representation of this set.
+     * @returns {Array}
+     */
+    ImmutableSet.prototype.toArray = function() {
+        var a = [];
+        this.forEach(function(v) { a.push(v); });
+        a.sort();
+        return a;
+    };
+
+    /**
+     * Return a string representation of this set.
+     * @returns {string}
+     */
+    ImmutableSet.prototype.toString = function() {
+        return "ImmutableSet(" + this.toArray() + ")";
+    };
+
+    /**
+     * Return a mutable copy of this set.
+     * @returns {Set}
+     */
+    ImmutableSet.prototype.asMutable = function() {
+        return new Set(this);
+    };
+
+    /**
+     * Return whether this set equals another set.
+     * @param {module:mpenc/helper/struct.ImmutableSet} other
+     * @returns {boolean}
+     */
+    ImmutableSet.prototype.equals = function(other) {
+        if (!other || other.size !== this.size) {
+            return false;
+        }
+        var eq = true;
+        this.forEach(function(v) {
+            if (!other.has(v)) {
+                eq = false;
+            }
+        });
+        return eq;
+    };
+
+    /**
+     * Return the disjunction of this and another set, i.e. elements that
+     * are in this <b>or</b> the other set.
+     * @param {module:mpenc/helper/struct.ImmutableSet} other
+     * @returns {module:mpenc/helper/struct.ImmutableSet}
+     */
+    ImmutableSet.prototype.union = function(other) {
+        var union = other.asMutable();
+        this.forEach(function(v) {
+            union.add(v);
+        });
+        return ImmutableSet(union);
+    };
+
+    /**
+     * Return the conjunction of this and another set, i.e. elements that
+     * are in this <b>and</b> the other set.
+     * @param {module:mpenc/helper/struct.ImmutableSet} other
+     * @returns {module:mpenc/helper/struct.ImmutableSet}
+     */
+    ImmutableSet.prototype.intersect = function(other) {
+        var intersection = new Set();
+        this.forEach(function(v) {
+            if (other.has(v)) {
+                intersection.add(v);
+            }
+        });
+        return ImmutableSet(intersection);
+    };
+
+    /**
+     * Return the difference of this and another set, i.e. elements that
+     * are in this <b>and not</b> the other set.
+     * @param {module:mpenc/helper/struct.ImmutableSet} other
+     * @returns {module:mpenc/helper/struct.ImmutableSet}
+     */
+    ImmutableSet.prototype.subtract = function(other) {
+        var difference = this.asMutable();
+        this.forEach(function(v) {
+            if (other.has(v)) {
+                difference.delete(v);
+            }
+        });
+        return ImmutableSet(difference);
+    };
+
+    /**
+     * Return what was [added, removed] between this and another set, i.e.
+     * same as [other.subtract(this), this.subtract(other)].
+     * @param {module:mpenc/helper/struct.ImmutableSet} newer
+     * @returns {module:mpenc/helper/struct.ImmutableSet[]}
+     */
+    ImmutableSet.prototype.diff = function(newer) {
+        return [newer.subtract(this), this.subtract(newer)];
+    };
+
+    /**
+     * Apply a difference to an older set.
+     *
+     * @param older {module:mpenc/helper/struct.ImmutableSet} Older set
+     * @param diff {module:mpenc/helper/struct.ImmutableSet[]} 2-tuple of what to (add, remove).
+     * @returns {module:mpenc/helper/struct.ImmutableSet} Newer set
+     */
+    ImmutableSet.prototype.patch = function(diff) {
+        if (!diff || diff[0].intersect(diff[1]).size > 0) {
+            throw new Error("invalid diff: " + diff);
+        }
+        return this.union(diff[0]).subtract(diff[1]);
+    };
+
+    /**
+     * Do a 3-way merge between this parent set and two child sets.
+     * @param {module:mpenc/helper/struct.ImmutableSet} first child
+     * @param {module:mpenc/helper/struct.ImmutableSet} other child
+     * @returns {module:mpenc/helper/struct.ImmutableSet} Result set
+     */
+    ImmutableSet.prototype.merge = function(child0, child1) {
+        return child1.patch(this.diff(child0));
+    };
+
+    Object.freeze(ImmutableSet.prototype);
     ns.ImmutableSet = ImmutableSet;
 
     return ns;
