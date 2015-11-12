a1c2e28b245c4e228e32b431c6ec5647bc278553
Ximin Luo <xl@mega.co.nz>
2015-11-12 14:23:50 +0100

    post #3016: fix test-static

--- test/build-test-static.js
+++ test/build-test-static.js
@@ -2,6 +2,13 @@
 
 // shim in browser global vars that some libs assume the existence of
 navigator = { appName: "nodejs" }; // jshint ignore:line
+location = { href: "" }; // jshint ignore:line
+crypto = { // jshint ignore:line
+    // this is dangerous, only for dev testing. never copy it into real code.
+    getRandomValues: function(a) {
+        a.forEach(function(v, i, a) { a[i] = 0; });
+    }
+};
 
 // load our library
 var mpenc = require(process.argv[2]);
